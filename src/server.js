51fda80610377d051c6244123fd46dd58ada9080
Habiba Sultana Bristy <bristysultana4@gmail.com>
2026-06-27 23:49:12 +0600

    MongoDB connection

---
+++ src/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("🟢 LegalEase server is running!");
+});
+
+// Connect to MongoDB, then start the server
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server is running on port ${PORT}`);
+  });
+});
