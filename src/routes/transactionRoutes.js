cc578edeb50d0a2a292fada95c770d0e300bcbea
Habiba Sultana Bristy <bristysultana4@gmail.com>
2026-07-16 22:00:25 +0600

    error

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -2,8 +2,3 @@ import express from "express";
 import { getAllTransactions } from "../controllers/transactionController.js";
 import { verifyToken, verifyRole } from "../middlewares/authMiddleware.js";
 
-const router = express.Router();
-
-router.get("/", verifyToken, verifyRole("admin"), getAllTransactions);
-
-export default router;
