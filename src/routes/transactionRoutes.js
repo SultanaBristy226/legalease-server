4c8666e464edfb2b83b428567bd3053c93f1e2e3
Habiba Sultana Bristy <bristysultana4@gmail.com>
2026-07-16 22:00:54 +0600

    fix code

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -2,3 +2,8 @@ import express from "express";
 import { getAllTransactions } from "../controllers/transactionController.js";
 import { verifyToken, verifyRole } from "../middlewares/authMiddleware.js";
 
+const router = express.Router();
+
+router.get("/", verifyToken, verifyRole("admin"), getAllTransactions);
+
+export default router;
